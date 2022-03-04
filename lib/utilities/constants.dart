95767188dfb98b3fd237be2acc69313cc376574a
Aabid <sayeedaabid0@gmail.com>
2022-03-04 23:42:42 +0530

    Weather Api Call with Beautiful UI

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 55.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+InputDecoration kTextFeildInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(color: Colors.grey),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide.none,
+  ),
+);
